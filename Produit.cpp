55127988e94461448566c9fb6dbb204a218e7dd1
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-05-01 02:34:59 +0100

    ajouter produit

--- Produit.cpp
+++ Produit.cpp
@@ -4,7 +4,8 @@
 using namespace std;
 
 //fct pour notre menu
-int menu(){
+int menu()
+{
 cout<<"***MENU***"<<endl;
 cout<<"1- Creation"<<endl;
 cout<<"2- Affichage"<<endl;
@@ -19,4 +20,29 @@ int choix;
 cin>>choix;
 
 return choix;
-};
+}
+//le & permet de transferer la reference de nb et non la copie
+void creation( Produit p[], int& nb)
+{
+    cout<<"Donner le nombre de produits";
+    cin>>nb;
+
+    for(int i=0;i<nb;i++){
+    testref:    cout<<"donner la reference";
+
+        //gestion de la cle primaire(refprod)
+        if(i==0){
+            cin>>p[i].refproduit;
+        }else{
+            string tmprefprod;
+            for(int j=0;j<i;j++){
+                if(p[j].refproduit==tmprefprod){
+                    cout<<"ce produit existe deja"<<endl;
+                    system("pause"); //pour maintenir le msg ci dessus d'etre a l'ecran
+                    goto testref;
+                }
+            }
+        }
+    }
+
+}
--- main.cpp
+++ main.cpp
@@ -5,15 +5,31 @@ using namespace std;
 int menu();
 int main()
 {
-    switch(menu()){
-    case 1: cout<<" vous avez choisit Creation"<<endl; break;
-    case 2: cout<<" vous avez choisit Affichage"<<endl; break;
-    case 3: cout<<" vous avez choisit Recherche"<<endl; break;
-    case 4: cout<<" vous avez choisit Suppression"<<endl; break;
-    case 5: cout<<" vous avez choisit rupture stock"<<endl; break;
-    case 6: cout<<" vous avez choisit Trie"<<endl; break;
-    case 7: cout<<" vous avez choisit de Quitter"<<endl; break;
-    default: cout<<" mauvais choix 1-7"<<endl;
+    switch(menu())
+    {
+    case 1:
+        cout<<" vous avez choisit Creation"<<endl;
+        break;
+    case 2:
+        cout<<" vous avez choisit Affichage"<<endl;
+        break;
+    case 3:
+        cout<<" vous avez choisit Recherche"<<endl;
+        break;
+    case 4:
+        cout<<" vous avez choisit Suppression"<<endl;
+        break;
+    case 5:
+        cout<<" vous avez choisit rupture stock"<<endl;
+        break;
+    case 6:
+        cout<<" vous avez choisit Trie"<<endl;
+        break;
+    case 7:
+        cout<<" vous avez choisit de Quitter"<<endl;
+        break;
+    default:
+        cout<<" mauvais choix 1-7"<<endl;
     }
     return 0;
 }
