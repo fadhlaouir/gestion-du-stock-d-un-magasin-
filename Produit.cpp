b5a2e816348a1386e33751e8cadab48cbbaef182
Fadhlaoui <fadhlaouiraied@hotmail.com>
2020-05-01 02:26:25 +0100

    Fix GNU prob

--- Produit.cpp
+++ Produit.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 //fct pour notre menu
 int menu(){
-count<<"***MENU***"<<endl;
-count<<"1- Création"<<endl;
-count<<"2- Affichage"<<endl;
-count<<"3- Recherche"<<endl;
-count<<"4- Suppression"<<endl;
-count<<"5- Produits en rupture stock"<<endl;
-count<<"6- Trie"<<endl;
-count<<"7- Quitter"<<endl;
-count<<"Faites votre choix"<<endl;
+cout<<"***MENU***"<<endl;
+cout<<"1- Creation"<<endl;
+cout<<"2- Affichage"<<endl;
+cout<<"3- Recherche"<<endl;
+cout<<"4- Suppression"<<endl;
+cout<<"5- Produits en rupture stock"<<endl;
+cout<<"6- Trie"<<endl;
+cout<<"7- Quitter"<<endl;
+cout<<"Faites votre choix"<<endl;
 
 int choix;
 cin>>choix;
 
 return choix;
-}
+};
--- main.cpp
+++ main.cpp
@@ -2,18 +2,18 @@
 #include "Produit.h"
 
 using namespace std;
-
+int menu();
 int main()
 {
     switch(menu()){
-    case 1: count<<" vous avez choisit Création"<<endl; break;
-    case 2: count<<" vous avez choisit Affichage"<<endl; break;
-    case 3: count<<" vous avez choisit Recherche"<<endl; break;
-    case 4: count<<" vous avez choisit Suppression"<<endl; break;
-    case 5: count<<" vous avez choisit Produits en rupture stock"<<endl; break;
-    case 6: count<<" vous avez choisit Trie"<<endl; break;
-    case 7: count<<" vous avez choisit Quitter"<<endl; break;
-    default: count<<" mauvais choix 1-7"<<endl;
+    case 1: cout<<" vous avez choisit Creation"<<endl; break;
+    case 2: cout<<" vous avez choisit Affichage"<<endl; break;
+    case 3: cout<<" vous avez choisit Recherche"<<endl; break;
+    case 4: cout<<" vous avez choisit Suppression"<<endl; break;
+    case 5: cout<<" vous avez choisit rupture stock"<<endl; break;
+    case 6: cout<<" vous avez choisit Trie"<<endl; break;
+    case 7: cout<<" vous avez choisit de Quitter"<<endl; break;
+    default: cout<<" mauvais choix 1-7"<<endl;
     }
     return 0;
 }
